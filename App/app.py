cd1d4a452093bb7e95af9842c1ccb6cebf6e60c3
wingorithm <erwin.gunawan@binus.ac.id>
2024-04-19 19:02:30 +0700

    final

--- App/app.py
+++ App/app.py
@@ -30,23 +30,7 @@ llm_base = HuggingFaceEndpoint(
 )
 
 vectara_client = initialize_vectara()
-# upload_all_file("../Bank Product Data/", vectara_client)
 
-# preprocess_prompt = PromptTemplate.from_template(
-#   """You are a professional and friendly CommonWealth Bank Customer Service. You must classify these user query as "commonwealth bank related question" or no!
-#     ANSWER MUST ONLY 1 word length EITHER "Yes" OR "No"!
-    
-#     user query : {issue}
-#     Classification:
-#     """
-# )
-
-# prompt = PromptTemplate.from_template(
-#   """You are a professional and friendly CommonWealth Bank Customer Service and you are helping a client with provided bank knowledge. 
-        
-#     user query: {issue} 
-#     """
-# )
 prompt = PromptTemplate.from_template(
   """You are a professional and friendly "CommonWealth" Bank Customer Service and you are helping a client with provided bank knowledge. 
   
@@ -63,19 +47,10 @@ prompt = PromptTemplate.from_template(
     """
 )
 
-# prompt2 = PromptTemplate.from_template(
-#   """You are a professional and friendly CommonWealth Bank Customer Service answer with human-like response and good manner. 
-#     DON'T answer in a long-winded way! Mke it Simple NO more than 3 sentences
+runnable = LLMChain(llm=llm_base, prompt=prompt)
+global_flag = False
 
-#     This is the Question: {issue}
-#     ANSWER :
-#     """
-# )
-
-runnable = prompt | llm_base | StrOutputParser()
-# runnable = LLMChain(llm=llm_base, prompt=prompt)
-# runnable = prompt | ChatOpenAI(streaming=True, callbacks=[StreamingStdOutCallbackHandler()], openai_api_key=OPENAI_API_KEY) | StrOutputParser()
-def generate_initial_message():
+def generate_initial_message(): 
     initial_prompt = "Welcome to CommonWealth Bank! How can I assist you today?"
     return initial_prompt
 
@@ -92,75 +67,32 @@ def generate_response_message(response):
     return full_response
 
 st.title("Bank Customer Service Chat")
+
 if "messages" not in st.session_state:
     st.session_state.messages = []
+    st.session_state.messages.append({
+        "role": "assistant",
+        "content": generate_initial_message()
+        })
 
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
+print(st.session_state.messages)
 
-initial_message = generate_response_message(generate_initial_message()) 
-st.session_state.messages.append({
-    "role": "assistant",
-    "content": initial_message
-})
 
 if user_input := st.chat_input("Enter your issue:"):
     st.session_state.messages.append({"role": "user", "content": user_input})
     with st.chat_message("user"):
         st.markdown(user_input)
 
-    #1st preprocessing step
-    # negative_words = ["no", "No.", "no.", "NO!", "NO", "no!"] 
-    # Classification = runnable.invoke({"issue": user_input})
-    # classification = Classification.lower()
-    # print("Cjeck => " + Classification)
-    
-    #2nd preprocessing step
+    print(st.session_state.messages)
     print("__________________ Start of knowledge content __________________")
     knowledge_content, summary = get_knowledge(user_input, vectara_client) #-> get documents from vectara Corpus
     print(summary)
-    print(knowledge_content)
-    response = runnable.invoke({"issue": user_input})
-    # response = runnable.invoke({"issue": user_input, "knowledge": summary, "adds_knowledge" : knowledge_content})
-    # if Classification.startswith("no"):
-    #     print("Other Information")
-    #     runnable1 = prompt2 | llm_base | StrOutputParser()
-    #     response = runnable1.invoke({"issue": user_input})
-    # else:
-    #     print("Bank Related Information")
-    
-    full_response = generate_response_message(response)
+    # print(knowledge_content)
+    response = runnable.invoke({"issue": user_input, "knowledge": summary, "adds_knowledge" : knowledge_content})
+
+    full_response = generate_response_message(str(response['text']))
+    print(full_response)
     st.session_state.messages.append({"role": "assistant", "content": full_response})
-
-# # Run when the submit button is pressed
-# if submit_button and uploaded_file:
-#     with tempfile.NamedTemporaryFile(delete=False, suffix=".txt") as tmpfile:
-#         tmpfile.write(uploaded_file.getvalue())
-#         tmp_filename = tmpfile.name
-
-#     try:
-#         vectara_client.add_files([tmp_filename])
-#         st.sidebar.success("txt file successfully uploaded to Vectara!")
-#     except Exception as e:
-#         st.sidebar.error(f"An error occurred: {str(e)}")
-#     finally:
-#         os.remove(tmp_filename)
-
-# def main(): 
-#     start_dependencies()    
-#     openai_api_key = os.getenv("OPENAI_API_KEY")
-#     llm = OpenAI(api_key=openai_api_key)
-#     retriever = vectara.as_retriever()
-    
-#     bot = ConversationalRetrievalChain.from_llm(
-#         llm, retriever, memory=memory, verbose=False
-#     )
-
-#     query = "is there a minimum deposit?"
-#     result = bot.invoke({"question": query})
-
-#     print(result["answer"])
-  
-# if __name__=="__main__": 
-#     main() 
