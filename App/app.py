11455c496afd8bfb32390dc2860cf5269c330f46
wingorithm <erwin.gunawan@binus.ac.id>
2024-04-19 19:41:59 +0700

    final2

--- App/app.py
+++ App/app.py
@@ -3,8 +3,8 @@ import tempfile
 import time
 from os.path import join, dirname
 from dotenv import load_dotenv
-dotenv_path = join(dirname(dirname(__file__)), '.env')
-load_dotenv(dotenv_path)
+# dotenv_path = join(dirname(dirname(__file__)), '.env')
+# load_dotenv(dotenv_path)
 
 import streamlit as st
 
